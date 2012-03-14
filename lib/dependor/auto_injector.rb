0e13e4a39a607749a325976cec5aee2313775063
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-03-14 17:11:56 +0100

    Extracted ClassNameResolver

--- lib/dependor/auto_injector.rb
+++ lib/dependor/auto_injector.rb
@@ -1,49 +1,35 @@
 module Dependor
   class AutoInjector
 
-    attr_reader :injector, :search_modules
-
     def initialize(injector, search_modules)
       @injector = injector
-      @search_modules = search_modules
       @instantiator = Instantiator.new(self)
+      @class_name_resolver = ClassNameResolver.new(search_modules)
     end
 
     def get(name)
-      return injector.send(name) if method_exists?(name)
-      raise UnknownObject.new("Injector does not know how to create object: #{name}") unless resolvable?(name)
+      ensure_resolvable!(name)
 
-      klass = class_for_object_name(name)
+      return @injector.send(name) if method_exists?(name)
+
+      klass = @class_name_resolver.for_name(name)
       @instantiator.instantiate(klass)
     end
 
     def resolvable?(name)
-      method_exists?(name) || !!class_for_object_name(name)
+      method_exists?(name) || !!@class_name_resolver.for_name(name)
     end
 
     private
 
-    def method_exists?(name)
-      injector.methods.include?(name)
-    end
-
-    def camelize(symbol)
-      string = symbol.to_s
-      string = string.gsub(/_\w/) { |match| match[1].upcase }
-      return string.gsub(/^\w/) { |match| match.upcase }
-    end
-
-    def class_for_object_name(name)
-      class_name = camelize(name)
-      modules = [Object, self].concat(search_modules)
-      klass = nil
-
-      modules.each do |mod|
-        klass = mod.const_get(class_name) rescue nil
-        break if klass
+    def ensure_resolvable!(name)
+      unless resolvable?(name)
+        raise UnknownObject.new("Injector does not know how to create object: #{name}")
       end
+    end
 
-      klass
+    def method_exists?(name)
+      @injector.methods.include?(name)
     end
 
   end
---
+++ spec/dependor/instantiator_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../spec_helper'
+
+describe Dependor::Instantiator do
+  let(:injector) { stub(:injector) }
+  let(:instantiator) { Dependor::Instantiator.new(injector) }
+
+  it "instantiates objects with no-arg constructors" do
+    klass = Class.new do
+      def foo
+        "foo"
+      end
+    end
+
+    instance = instantiator.instantiate(klass)
+    instance.foo.should == "foo"
+  end
+
+  it "instantiates objects with constructors" do
+    klass = Class.new do
+      def initialize(foo, bar, baz)
+        @foo = [foo, bar, baz].join('-')
+      end
+
+      def foo
+        @foo
+      end
+    end
+
+    injector.should_receive(:get).with(:foo).and_return("foo")
+    injector.should_receive(:get).with(:bar).and_return("bar")
+    injector.should_receive(:get).with(:baz).and_return("baz")
+
+    instance = instantiator.instantiate(klass)
+
+    instance.foo.should == 'foo-bar-baz'
+  end
+end
